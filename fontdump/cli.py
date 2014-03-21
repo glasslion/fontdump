cf0494a75a11fd6b1d70e736681ee1380e338217
Leonardo.Z <glasslion@gmail.com>
2014-03-21 13:46:45 +0800

    Init

---
+++ fontdump/cli.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-#
+import requests
+import cssutils
+
+USER_AGENTS = {
+    'woff': 'Mozilla/5.0 AppleWebKit/537.36 Chrome/30', # Chrome
+    'eot': 'Mozilla/4.0 (compatible; MSIE 6.0;)', # IE6
+    'ttf': 'Mozilla/5.0 (Linux; U; Android 2.1-update1;)', #Andord 2
+    'svg': 'Mozilla/4.0 (iPad; CPU OS 4_0_1 ) AppleWebKit', #iOS<4.2
+}
+
+def main():
+    font_url = 'http://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800|Dosis:300,400'
+    sheets={}
+    for (format, ua) in USER_AGENTS.items():
+        headers = {
+            'User-Agent': ua,
+        }
+
+        r =requests.get(font_url, headers=headers)
+        sheets[format] = cssutils.parseString(r.content)
+
+
+if __name__ == '__main__':
+    main()
