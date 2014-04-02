717af3e80cb0b1d07f14a4ae7e39610fc95bb86b
Leonardo.Z <glasslion@gmail.com>
2014-04-02 13:25:55 +0800

    More test.

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+
 import six
+import pytest
 
 from fontdump.core import GoogleFontGroup
 class TestMultipleWeightGroup:
@@ -24,9 +27,15 @@ class TestMultipleWeightGroup:
 
 class TestSingleWeightSingleFont(object):
     @classmethod
-    def setup_class(cls):
+    @pytest.fixture(autouse=True)
+    def setup_class(cls, tmpdir):
+        cls.tmpdir = tmpdir
+        cls.font_dir = tmpdir.mkdir('font_dir')
+        cls.output_dir = tmpdir.mkdir('output_dir')
         cls.group = GoogleFontGroup(
-            'http://fonts.googleapis.com/css?family=Open+Sans:400'
+            'http://fonts.googleapis.com/css?family=Open+Sans:400',
+            str(cls.font_dir),
+            str(cls.output_dir)
             )
         cls.font = cls.group.fonts[0]
 
@@ -45,3 +54,14 @@ class TestSingleWeightSingleFont(object):
 
     def test_has_ie_fix(self):
         assert self.group.has_ie_fix == False
+
+    def test_font_dir_path(self):
+        assert str(self.font_dir) == self.group.font_dir_path
+
+    def test_download_font_files(self):
+        self.font.download_font_files()
+        output_dir = str(self.output_dir)
+        font_name = self.font.primary_name
+        for format in self.group.formats:
+            assert os.path.exists(
+                os.path.join(output_dir, '%s.%s' % (font_name,format)))
